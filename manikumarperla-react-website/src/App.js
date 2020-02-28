41514487922c24e2de05debeb49a2deb08d0c5f0
Manikumar1998 <manikumarperla18@gmail.com>
2020-02-28 22:22:55 +0900

    Add Tech stack

--- manikumarperla-react-website/src/App.js
+++ manikumarperla-react-website/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./Components/Header";
 import Title from "./Components/Title";
+import SkillStack from "./Components/SkillStack";
 import Footer from "./Components/Footer";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         name="Manikumar"
         lead="I am a software engineer at LINE Corporation"
       />
+      <SkillStack />
       <Footer />
     </div>
   );
---
+++ manikumarperla-react-website/src/Components/Skill.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import uuid from "uuid";
+
+class Skill extends Component {
+  render() {
+    const { image, alt, totalStars, activeStars } = this.props.skill;
+    const rating = [];
+    for (let i = 0; i < totalStars; i++) {
+      if (i < activeStars) {
+        rating.push(
+          <span key={uuid()} className="text-info">
+            {" "}
+            ★{" "}
+          </span>
+        );
+      } else {
+        rating.push(<span key={uuid()}> ★ </span>);
+      }
+    }
+    const skillStyle = {
+      width: "100px",
+      height: "100px"
+    };
+
+    return (
+      <div>
+        <img
+          className="rounded-circle img-fluid"
+          src={require(`../assets/${image}`)}
+          alt={alt}
+          style={skillStyle}
+        />
+        <div className="justify-content-around"> {rating} </div>
+      </div>
+    );
+  }
+}
+
+export default Skill;

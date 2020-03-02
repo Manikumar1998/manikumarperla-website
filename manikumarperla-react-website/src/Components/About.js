472c5ca8b8d2334a4397288dd19b18f10de7e6eb
Manikumar1998 <manikumarperla18@gmail.com>
2020-03-03 00:58:49 +0900

    class -> className

--- manikumarperla-react-website/src/Components/About.js
+++ manikumarperla-react-website/src/Components/About.js
@@ -2,42 +2,42 @@ import React from "react";
 
 export default function About() {
   return (
-    <div class="container-fluid bg-light my-3">
-      <div class="container text-center py-5">
-        <h1 class="text-center font-weight-light">
-          <span class="text-info">About</span> me
+    <div className="container-fluid bg-light my-3">
+      <div className="container text-center py-5">
+        <h1 className="text-center font-weight-light">
+          <span className="text-info">About</span> me
         </h1>
-        <div class="lead">I am full stack developer from India</div>
-        <div class="row">
-          <div class="col-12 col-sm-6 px-5 pt-5">
+        <div className="lead">I am full stack developer from India</div>
+        <div className="row">
+          <div className="col-12 col-sm-6 px-5 pt-5">
             <h5>What I can do?</h5>
-            <p class="text-justify">
+            <p className="text-justify">
               I love writing code, hands down. I wrote my first program right
               after my 10th grade. Just like 99.99% of all the programmers, my
               first program was indeed the holy “Hello world!” program.
             </p>
           </div>
-          <div class="col-12 col-sm-6 px-5 pt-5">
+          <div className="col-12 col-sm-6 px-5 pt-5">
             <h5>What am I doing currently?</h5>
-            <p class="text-justify">
+            <p className="text-justify">
               I love writing code, hands down. I wrote my first program right
               after my 10th grade. Just like 99.99% of all the programmers, my
               first program was indeed the holy “Hello world!” program.
             </p>
           </div>
         </div>
-        <div class="row">
-          <div class="col-12 col-sm-6 px-5 pt-5">
+        <div className="row">
+          <div className="col-12 col-sm-6 px-5 pt-5">
             <h5>What do I believe in?</h5>
-            <p class="text-justify">
+            <p className="text-justify">
               I love writing code, hands down. I wrote my first program right
               after my 10th grade. Just like 99.99% of all the programmers, my
               first program was indeed the holy “Hello world!” program.
             </p>
           </div>
-          <div class="col-12 col-sm-6 px-5 pt-5">
+          <div className="col-12 col-sm-6 px-5 pt-5">
             <h5>How I can help you?</h5>
-            <p class="text-justify">
+            <p className="text-justify">
               I love writing code, hands down. I wrote my first program right
               after my 10th grade. Just like 99.99% of all the programmers, my
               first program was indeed the holy “Hello world!” program.
